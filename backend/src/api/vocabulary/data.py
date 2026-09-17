1fe464d1ff8fca3d0babe32d478b41646f137e87
Agtdbx <auguste.deroubaix@gmail.com>
2026-09-17 18:35:02 +0200

    vocabulary endpoint : add kana for verbs and adjectives, add type for tenses

--- backend/src/api/vocabulary/data.py
+++ backend/src/api/vocabulary/data.py
@@ -1,3 +1,5 @@
+import re
+import jaconv
 from jpconjugation.define import (
     VERBS_TENSES, VERBS_ALLOWED_FORMS, VERBS_CONJUGATION_ORDER,
     ADJECTIVES_TENSES, ADJECTIVES_ALLOWED_FORMS, ADJECTIVES_CONJUGATION_ORDER,
@@ -41,12 +43,19 @@ def _get_words(
     for word in words:
         words_info.append({
             "romaji" : word.romaji,
+            "kana" : _word_to_kana(word),
             "kanji" : word.kanji,
             "traduction" : word.traduction,
             "type" : word.type,
         })
 
     return words_info
+
+
+def _word_to_kana(word: Verb | Adjective) -> str:
+    if bool(re.search(r'[\u4E00-\u9FFF]', word.romaji)):
+        return jaconv.alphabet2kana(word.romaji)
+    return word.kanji
 
 
 def _get_tenses(
@@ -61,6 +70,8 @@ def _get_tenses(
     verb_exception_suru = Verb(romaji="suru", kanji="する", traduction="faire", type="exception", stem="su", ending="ru")
     adjective_i = Adjective(romaji="furui", kanji="古い", traduction="vieux", type="i", stem="furu")
     adjective_na = Adjective(romaji="kirei", kanji="綺麗", traduction="joli", type="na", stem="kirei")
+
+    tense_type = "verbs" if verb else "adjectives"
 
     tenses_info = []
     for tense_id, tense_name in tenses.items():
@@ -81,7 +92,8 @@ def _get_tenses(
             "allowedForms": allowed_forms.get(tense_id, []),
             "order": conjugation_orders.get(tense_id, -1),
             "info": info.get(tense_id, "Pas de description."),
-            "rules": rules
+            "rules": rules,
+            "type": tense_type,
         })
 
     return tenses_info
